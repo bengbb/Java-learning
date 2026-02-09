f744b11d2e44c19de5cf3467762118a08162e40a
bengbb <1040191545@qq.com>
2026-02-09 23:31:44 +0800

    注释

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package ABC;
+//package表示当前文件所在的包
+//class表示 类
+//Helloworld表示 类名
+public class HelloWorld {
+    //程序的主入口，固定格式
+    public static void main() {
+        //输出语句
+        System.out.println("Hello World");
+
+    }
+}
