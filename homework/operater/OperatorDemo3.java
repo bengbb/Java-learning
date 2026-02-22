197860096ab63d478931b3c29f454c0dd72db744
bengbb <1040191545@qq.com>
2026-02-22 23:24:41 +0800

    Add files via upload

---
+++ homework/operater/OperatorDemo3.java
@@ -0,0 +1,24 @@
+package operator;
+
+import java.util.Scanner;
+
+public class OperatorDemo3 {
+    public static void main(String[] args){
+        //键盘输入一个四位数，判断是不是回文数。
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个四位数：");
+        int num = sc.nextInt();
+        int ge = num % 10;
+        int shi = num / 10 % 10;
+        int bai = num / 100 % 10;
+        int qian = num / 1000;
+        if(ge == qian && shi == bai){
+            System.out.println("是回文数");
+        }
+        else{
+            System.out.println("不是回文数");
+        }
+    }
+
+
+}
